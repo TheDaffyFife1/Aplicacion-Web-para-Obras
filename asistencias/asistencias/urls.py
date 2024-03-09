fdc49824e84bd5d464b1b019c3e21a8d4a722a5b
TheDaffyFife <castillotorresoscar@gmail.com>
2024-03-08 18:11:33 -0600

    arregllos y agregacion de templates

--- asistencias/asistencias/urls.py
+++ asistencias/asistencias/urls.py
@@ -19,7 +19,9 @@ from asistencias_obras.views import accesos
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('', auth_views.LoginView.as_view(template_name='login.html'), name='login'),  # Redirige la ruta raíz al login
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
-    path('admin/', admin.site.urls),
+    path('dashboard/', accesos, name='dashboard'),
+    # Otras rutas de tu aplicación
 ]
